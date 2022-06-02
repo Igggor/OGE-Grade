f5de0fe2a8a03ec3229da1b31ef2da26f284e288
Igor <82223725+Igggor@users.noreply.github.com>
2022-06-02 19:48:34 +0300

    Update CheckingINFO.py

--- CheckingINFO.py
+++ CheckingINFO.py
@@ -5,5 +5,5 @@ Grade = "н/д"
 while Grade == "н/д":
     Grade = Check_my_grade()
     print(Grade)
-    sleep(10)
+    sleep(10) #в функцию sleep установите время ожидания перед слдедующей проверкой в секундах. Лично я рекомендую 5 минут sleep(300)
 print(Grade)
